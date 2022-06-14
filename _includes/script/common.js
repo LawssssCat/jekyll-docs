b66d51f69f9d11ba8d5be9a8cc8272cfd8f92464
LawssssCat <1191693505@qq.com>
2022-06-14 15:27:32 +0800

    build: eslint

---
+++ _includes/script/common.js
@@ -0,0 +1,4 @@
+(function() {
+  var a = console;
+  a.log('hello world');
+})();
--- commitlint.config.js
+++ commitlint.config.js
@@ -5,37 +5,37 @@ module.exports = {
   parserPreset: 'conventional-changelog-conventionalcommits',
   // see https://commitlint.js.org/#/reference-rules
   rules: {
-		'body-leading-blank': [1, 'always'],
-		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 72],
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
-		'subject-case': [
-			2,
-			'never',
-			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-		],
-		'subject-empty': [2, 'never'],
-		'subject-full-stop': [2, 'never', '.'],
-		'type-case': [2, 'always', 'lower-case'],
-		'type-empty': [2, 'never'],
-		'type-enum': [
-			2,
-			'always',
-			[
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
         'build',
-				'chore',
-				'ci',
-				'docs',
-				'feat',
-				'fix',
-				'improvement',
-				'perf',
-				'refactor',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'improvement',
+        'perf',
+        'refactor',
         'release',
-				'revert',
-				'style',
-				'test'
-			]
-		]
-	}
-}
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+};
