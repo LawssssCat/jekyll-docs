f0702917e4efbb1a9c2694f7092db0e33115bd9d
LawssssCat <1191693505@qq.com>
2022-07-07 22:06:23 +0800

    improvement: code header createCodeHeader

--- _javascript/commons/code-header.js
+++ _javascript/commons/code-header.js
@@ -7,27 +7,38 @@ lazyload.onload(() => {
   window.document.querySelectorAll(tagRenderSelector).forEach(dom => {
     const code = dom.querySelector('code');
     if(code) {
-      const header = window.document.createElement('div');
-      dom.prepend(header);
-
-      const headerLang = window.document.createElement('div');
-      header.appendChild(headerLang);
       const lang = code.getAttribute('data-lang');
-      headerLang.innerHTML = lang;
-
-      const headerCopy = window.document.createElement('div');
-      header.appendChild(headerCopy);
-      headerCopy.innerHTML = 'Copy';
-      headerCopy.addEventListener('click', () => {
-        TOOL.copyTextToClipboard(dom.textContent);
-      });
-
+      createCodeHeader(dom, code, lang);
     }
   });
   // <div class="language-javascript highlighter-rouge"><div class="highlight"><pre class="highlight"><code>...</code>
   const kramRenderSelector='.highlighter-rouge';
   window.document.querySelectorAll(kramRenderSelector).forEach(dom => {
-
+    const code = dom.querySelector('code');
+    if(code) {
+      const className = Array.from(dom.classList).find(item => {
+        return item.startsWith('language-');
+      });
+      const lang = className.replace('language-', '');
+      createCodeHeader(dom, code, lang);
+    }
   });
 });
 
+function createCodeHeader(dom, code, lang) {
+  const header = window.document.createElement('div');
+  dom.prepend(header);
+
+  const headerLang = window.document.createElement('div');
+  header.appendChild(headerLang);
+  headerLang.innerHTML = lang;
+
+  const headerCopy = window.document.createElement('div');
+  header.appendChild(headerCopy);
+  headerCopy.innerHTML = 'Copy';
+  headerCopy.addEventListener('click', () => {
+    TOOL.copyTextToClipboard(dom.textContent).then(() => {
+      console.log('copy ok');
+    });
+  });
+}
