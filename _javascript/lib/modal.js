a8e18c9c210583456a0e326f916989a718b32fea
LawssssCat <1191693505@qq.com>
2022-07-15 14:41:31 +0800

    refactor(modal): change 'mask' to modal

---
+++ _javascript/lib/modal.js
@@ -0,0 +1,30 @@
+class Modal {
+  constructor(options={}) {
+    this.container         = options.container          || window.document.body;
+    const config = this.config = {};
+    config.modalClass      = options.modalClass         || 'modal';
+    config.modalShowClass  = options.modalShowClass     || 'modal--show';
+    this.hideCallback      = options.hideCallback;
+    this.showCallback      = options.showCallback;
+    // node
+    this.node = window.document.createElement('div');
+    this.node.classList.add(this.config.modalClass);
+  }
+  show() {
+    this.node.classList.add(this.config.modalShowClass);
+    this.container.appendChild(this.node);
+    this.showCallback && this.showCallback();
+  }
+  hide() {
+    this.node.classList.remove(this.config.modalShowClass);
+    this.container.removeChild(this.node);
+    this.hideCallback && this.hideCallback();
+  }
+  addEventListener(...args) {
+    this.node.addEventListener(...args);
+  }
+}
+
+module.exports = {
+  Modal
+};
