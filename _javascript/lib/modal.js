a614ca5ba8665c53843ee6b117698dc68f86f0d8
LawssssCat <1191693505@qq.com>
2022-07-15 15:11:07 +0800

    improvement(modal): container overflowY

--- _javascript/lib/modal.js
+++ _javascript/lib/modal.js
@@ -10,12 +10,21 @@ class Modal {
     this.node = window.document.createElement('div');
     this.node.classList.add(this.config.modalClass);
   }
+  scrollDisable() {
+    this.containerOverflowY = this.container.style.overflowY;
+    this.container.style.overflowY = 'hidden';
+  }
+  scrollEnable() {
+    this.container.style.overflowY = this.containerOverflowY || '';
+  }
   show() {
+    this.scrollDisable();
     this.node.classList.add(this.config.modalShowClass);
     this.container.appendChild(this.node);
     this.showCallback && this.showCallback();
   }
   hide() {
+    this.scrollEnable();
     let TransitionedFunc;
     this.node.addEventListener('transitionend', TransitionedFunc = () => {
       this.container.removeChild(this.node);
